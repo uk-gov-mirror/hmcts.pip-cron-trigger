d30482c4b13d9ac452f69ea9c8aaaf820f248114
kian.kwa <kian.kwa@justice.gov.uk>
2026-03-06 12:32:48 +0000

    Add missing annotation

--- src/main/java/uk/gov/hmcts/reform/pip/cron/trigger/triggers/ScheduledListTypeSubscriptionsTrigger.java
+++ src/main/java/uk/gov/hmcts/reform/pip/cron/trigger/triggers/ScheduledListTypeSubscriptionsTrigger.java
@@ -2,11 +2,13 @@ package uk.gov.hmcts.reform.pip.cron.trigger.triggers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import uk.gov.hmcts.reform.pip.cron.trigger.model.ScheduleTypes;
 
 import static org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction.clientRegistrationId;
 
+@Service
 public class ScheduledListTypeSubscriptionsTrigger implements Trigger {
     WebClient webClient;
 
